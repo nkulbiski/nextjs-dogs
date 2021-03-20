40cf570c17a02da0751729e2cf3697338801901a
Neil Kulbiski <neil.kulbiski@backlotcars.com>
2021-03-20 14:14:57 -0500

    add infinite scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,31 @@
 import { useSWRInfinite } from 'swr';
 import Card from 'components/dogs/card';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 
 export default function Home() {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const getKey = (pageIndex: number) => (`https://dog.ceo/api/breeds/image/random/15?index=${pageIndex}`);
 
-  const { data, size, setSize } = useSWRInfinite(getKey, fetcher, { revalidateOnFocus: false });
+  const {
+    data, size, setSize, isValidating,
+  } = useSWRInfinite(getKey, fetcher, { revalidateOnFocus: false });
+
+  const isRefreshing = isValidating && data && data.length === size;
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && !isRefreshing) { setSize(size + 1); }
+  }, [inView, isRefreshing]);
 
   if (!data) return 'Loading...';
   return (
     <>
       <div>
         {data.map((dogs) => dogs.message.map((image:string) => <Card image={image} key={image} />))}
-
       </div>
-      <button type="button" onClick={() => setSize(size + 1)}>Load More</button>
+      <div ref={ref} />
     </>
   );
 }
