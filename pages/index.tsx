5d9eb7c62b566327cc016be44ededb47b2439fe8
Neil Kulbiski <neil.kulbiski@backlotcars.com>
2021-03-20 09:19:37 -0500

    fix reloading earlier dogs bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Card from 'components/dogs/card'
 
 export default function Home() {
   const fetcher = (url: string) => fetch(url).then(r => r.json())
-  const getKey = () => ('https://dog.ceo/api/breeds/image/random/15')
+  const getKey = (pageIndex: number) => (`https://dog.ceo/api/breeds/image/random/15?index=${pageIndex}`)
+
   const { data, size, setSize } = useSWRInfinite(getKey, fetcher, { revalidateOnFocus: false })
 
   if (!data) return 'Loading...'
