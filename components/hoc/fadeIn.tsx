4447e827816012ef527cb9344781f656a3cd9afb
Neil Kulbiski <neil.kulbiski@backlotcars.com>
2021-03-28 12:47:54 -0500

    faster default fadein duration

--- components/hoc/fadeIn.tsx
+++ components/hoc/fadeIn.tsx
@@ -16,7 +16,7 @@ const fadeIn = keyframes`
 `;
 
 const Wrapper = styled.div`
-  animation: ${fadeIn} ${({ delay = 12 }:IFadeIn) => `${delay}s`};
+  animation: ${fadeIn} ${({ delay = 2 }:IFadeIn) => `${delay}s`};
 `;
 
 export default (WrappedComponent: React.ComponentType<any>) => (props: any) => (
