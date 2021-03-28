af1fc427ffaaa7aacb83c01ba050277cef006021
Neil Kulbiski <neil.kulbiski@backlotcars.com>
2021-03-28 12:43:37 -0500

    preload dog images

--- components/dogs/card.tsx
+++ components/dogs/card.tsx
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
 import Caption from 'components/dogs/caption';
-import Image from 'next/image';
-import fadeIn from 'components/animation/fadeIn';
+import NextImage from 'next/image';
+import fadeIn from 'components/hoc/fadeIn';
+import PreloadImage from 'components/hoc/preloadImage';
 
 export interface IDogCard {
     image: string,
     className?: string
 }
 
-const FadeInDogImage = fadeIn(Image);
+const DogImage = fadeIn(PreloadImage(NextImage));
 
 const card = ({ className, image }:IDogCard) => (
   <div className={className}>
-    <FadeInDogImage src={image} width="600px" height="600px" objectFit="cover" priority />
+    <DogImage src={image} width="600px" height="600px" objectFit="cover" priority />
     <Caption />
   </div>
 );
